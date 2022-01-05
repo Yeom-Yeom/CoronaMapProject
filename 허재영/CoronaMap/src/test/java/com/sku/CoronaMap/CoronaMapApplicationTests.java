eccbd1ce1d7e44dd7387e2d52b0b9b19707d3304
heoj10272 <heoj10272@naver.com>
2022-01-05 22:20:23 +0900

    it's me

---
+++ 허재영/CoronaMap/src/test/java/com/sku/CoronaMap/CoronaMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sku.CoronaMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
