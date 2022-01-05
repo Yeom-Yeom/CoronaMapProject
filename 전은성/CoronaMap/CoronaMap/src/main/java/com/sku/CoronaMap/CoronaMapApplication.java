f8dfd2a3162b95ea616f7adc0cb21ea1d6dc3776
jjjes758 <jjjes758@skuniv.ac.kr>
2022-01-05 22:22:34 +0900

    전은성

---
+++ 전은성/CoronaMap/CoronaMap/src/main/java/com/sku/CoronaMap/CoronaMapApplication.java
@@ -0,0 +1,13 @@
+package com.sku.CoronaMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaMapApplication.class, args);
+	}
+
+}
