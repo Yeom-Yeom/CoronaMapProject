29410a241dad8bc2974884bd4a9a5323a82f044a
HeeSung98 <wlfkrdl2@gmail.com>
2022-01-08 23:14:38 +0900

    22.01.08 hs

---
+++ 윤희성/CoronaMap/src/main/java/com/sku/CoronaMap/controller/CoronaController.java
@@ -0,0 +1,30 @@
+package com.sku.CoronaMap.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CoronaController {
+    @GetMapping("corona")
+    public String corona(Model model)
+    {
+        return "corona";
+    }
+    @GetMapping("corona/statistics")
+    public String statistics(Model model)
+    {
+        return "statistics";
+    }
+    @GetMapping("corona/find")
+    public String find(Model model)
+    {
+        return "find";
+    }
+    @GetMapping("corona/board")
+    public String board(Model model)
+    {
+        return "board";
+    }
+}
