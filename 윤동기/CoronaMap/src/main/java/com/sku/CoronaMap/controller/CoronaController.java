2054f8f7673577531287c214071f2fb9a52a36ba
ydg010 <ydg010@skuniv.ac.kr>
2022-01-08 23:11:34 +0900

    ydg

---
+++ 윤동기/CoronaMap/src/main/java/com/sku/CoronaMap/controller/CoronaController.java
@@ -0,0 +1,32 @@
+package com.sku.CoronaMap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CoronaController {
+    @GetMapping("corona")
+    public String corona(Model model){
+        return "corona";
+    }
+
+    @GetMapping("corona/statistics")
+    public String coronastatistics(Model model){
+        return "statistics";
+    }
+
+    @GetMapping("corona/find")
+    public String coronafind(Model model){
+        return "find";
+    }
+    @GetMapping("corona/board")
+    public String coronaboard(Model model){
+        return "board";
+    }
+
+
+
+
+
+}
